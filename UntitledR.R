a3d33115f7e17a8679ff75b521970e10bcde426c
Zazni Vlijter <zaznivlijter@gwu.edu>
2023-03-28 23:45:14 +0000

    inital commit

---
+++ UntitledR.R
@@ -0,0 +1,26 @@
+---
+  title: "Zazni Vlijter"
+about:
+  template: jolla
+image: 293A8DB3-C4AF-42EE-AF1D-91E6EC88FCC7.jpg
+links:
+  - icon: twitter
+text: twitter
+href: https://twitter.com/akesslerdc
+- icon: github
+text: Github
+href: https://github.com/amkessler
+---
+  
+  Hi, I'm Zazni and I'm a student at The George Washington University. I like to cook and take pictures!
+  
+  ## Education
+  
+  The George Washington University, Bachelor of Arts in English
+
+## Skills
+
+Writing,
+Strategic Communications,
+Design, and
+Photography 
